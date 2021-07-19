139727737eb2b111bcfb7e1713ecd775d6a82eaf
indrajeet-kadam <indrajeet.javatech@gmail.com>
2021-07-20 01:05:41 +0530

    Resolved Test Cases Issue

--- src/test/java/com/jenkins/api/SpringJenkinsApplicationTests.java
+++ src/test/java/com/jenkins/api/SpringJenkinsApplicationTests.java
@@ -1,15 +1,15 @@
 package com.jenkins.api;
 
-import static org.junit.Assert.*;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 class SpringJenkinsApplicationTests {
 
@@ -18,7 +18,7 @@ class SpringJenkinsApplicationTests {
 	@Test
 	void contextLoads() {
 		logger.info("Test Case executing...");
-		assertEquals(true, true);
+		Assertions.assertEquals(true, true);
 	}
 
 }
